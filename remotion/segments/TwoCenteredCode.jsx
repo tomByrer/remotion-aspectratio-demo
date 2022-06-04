d1b215f82aa728ed06a396ddb7557f772209bd64
tomByrer <tombyrer@gmail.com>
2022-06-04 05:33:23 -0500

    + TwoCenteredCode segment

---
+++ remotion/segments/TwoCenteredCode.jsx
@@ -0,0 +1,93 @@
+import {interpolate, Sequence, useCurrentFrame, useVideoConfig} from 'remotion';
+
+import * as useConvert from '../helpers/useConvert'
+
+// import './FrameCount.css'
+
+export const TwoCenteredCode = ({
+	aspects={
+		colorBack: '#111',
+		colorFront: '#eee',
+		colorFrontCenter: null,
+		fontVWRatio: 9,
+		placeItems: 'center',
+		textTop: 'top',
+		codeLeft: 'code left',
+		codeRight: 'code right',
+		textLeft: 'text left',
+		textRight: 'text right',
+		timeDurSecs: 2,
+	}
+}) => {
+	const frame = useCurrentFrame();
+	const videoConfig = useVideoConfig();
+
+	const opacity = interpolate(
+		frame,
+		[videoConfig.durationInFrames - 25, videoConfig.durationInFrames - 15],
+		[1, 0],
+		{
+			extrapolateLeft: 'clamp',
+			extrapolateRight: 'clamp',
+		}
+	);
+
+	return (
+		<div className='seq-frame-count' style={{
+			minHeight: useConvert.vh(),
+			minWidth: useConvert.vw(),
+			display: "grid",
+			gridTemplateColumns: "1fr 1fr",
+			gridTemplateRows: "1fr 1fr 1fr",
+			gap: "0 0",
+			gridAutoFlow: "row",
+			gridTemplateAreas:
+				`'textTop textTop'
+				'codeLeft codeRight'
+				'textLeft textRight'`,
+			gridAutoRows: '1fr',
+			placeItems: aspects.placeItems,
+			textAlign: "center",
+			backgroundColor: aspects.colorBack,
+		}}>
+			<div
+				style={{
+					gridArea: 'textTop',
+					color: aspects.colorFront,
+					fontSize: useConvert.sizeFont(61.8),
+			}}>
+				{aspects.textTop}
+			</div>
+			<code
+				style={{
+					color: aspects.colorFrontCenter ?? aspects.colorFront,
+					fontSize: useConvert.sizeFont(100),
+				}}
+			>
+				{aspects.codeLeft}
+			</code>
+			<code
+				style={{
+					color: aspects.colorFrontCenter ?? aspects.colorFront,
+					fontSize: useConvert.sizeFont(100),
+				}}
+			>
+				{aspects.codeRight}
+			</code>
+			<div
+				style={{
+					color: aspects.colorFront,
+					fontSize: useConvert.sizeFont(61.8),
+			}}>
+				{aspects.textLeft}
+			</div>
+			<div
+				style={{
+					color: aspects.colorFront,
+					fontSize: useConvert.sizeFont(61.8),
+			}}>
+				{aspects.textRight}
+			</div>
+		</div>
+	);
+};
