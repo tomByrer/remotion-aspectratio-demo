2a1edbe5d234bb649092c55e88c2ede81b170a83
tomByrer <tombyrer@gmail.com>
2022-06-20 17:08:55 -0500

    refactor: CompositionCollection

--- remotion/CompositionCollection.jsx
+++ remotion/CompositionCollection.jsx
@@ -4,9 +4,6 @@ import * as settings from "./helpers/settings"
 import { prep } from './helpers/prep-transcript'
 import { ArrangeSeries } from "./arrangements/ArrangeSeries"
 import { NiceComposition } from './helpers/Helper'
-import {HelloWorld} from './HelloWorld';
-import {Logo} from './HelloWorld/Logo';
-import {Subtitle} from './HelloWorld/Subtitle';
 import { TitleOutlined } from './parts/TitleOutlined'
 
 import { transcriptTimeTest } from '../remotion/transcripts/time-test'
@@ -16,6 +13,16 @@ export const CompositionCollection = () => {
 	let vidKey = 'square'
 	let vidSize = preset.vidSizes[vidKey]
 	let preparedTR = prep(transcriptTimeTest)
+
+	let
+		component = TitleOutlined,
+		durationInSeconds = 3,
+		durationInFrames = durationInSeconds * preset.fps,
+		width = (vidSize.dimention.w) || 1920,
+		height = (vidSize.dimention.h) || 1080,
+		// defaultProps=defaultProps??{},
+		id = `${component.name}-${width}x${height}-${preset.fps}fps`;
+
 	return (
 		<>
 			<Composition
@@ -30,9 +37,20 @@ export const CompositionCollection = () => {
 					style: {color: 'orange'},
 				}}
 			/>
+			{/* <Composition
+				id={id}
+				component={TitleOutlined}
+				durationInFrames={durationInFrames}
+				fps={preset.fps}
+				width={width}
+				height={height}
+				defaultProps={{
+					displayText: 'Comp variable Test Text',
+					style: {color: 'brown'},
+				}}
+			/> */}
 
 			<NiceComposition
-				id='TitleOutlined-360x360-16fps'
 				component={TitleOutlined}
 				defaultProps={{
 					displayText: 'NiceComposition Test Text',
@@ -43,41 +61,6 @@ export const CompositionCollection = () => {
 				vidKey='square'
 			/>
 
-			{/* <Composition
-				id="TitleOutlined"
-				component={ArrangeSeries}
-				durationInFrames={preparedTR.tr.at(-1).timeEndFrame}
-				fps={preset.fps}
-				width={vidSize.dimention.w}
-				height={vidSize.dimention.w}
-				defaultProps={{
-					transcript: preparedTR.tr,
-				}}
-			/> */}
-			{/* <Composition
-				id="HelloWorld"
-				component={HelloWorld}
-				durationInFrames={150}
-				fps={30}
-				width={1080}
-				height={1080}
-				defaultProps={{
-					backgroundColor: '#131C20',
-					titleText: 'Welcome to Remotion',
-					titleColor: '#86A8E7',
-					subtitleText: 'Animated videos using ReactJS',
-					subtitleColor: '#91EAE4',
-					test: 'test ignire me',
-				}}
-			/>
-			<Composition
-				id="Logo"
-				component={Logo}
-				durationInFrames={200}
-				fps={30}
-				width={1080}
-				height={1080}
-			/> */}
 			<Composition
 				id="TitleOutlined"
 				component={TitleOutlined}
--- remotion/render.mjs
+++ remotion/render.mjs
@@ -2,6 +2,8 @@ import path from "path";
 import { bundle } from "@remotion/bundler";
 import { getCompositions, renderMedia } from "@remotion/renderer";
 
+// import { Title } from './parts/Title'
+
 const start = async () => {
   // The composition you want to render
   const compositionId = "Title";
@@ -11,39 +13,40 @@ const start = async () => {
   console.log("Creating a Webpack bundle of the video");
   const bundleLocation = await bundle(path.resolve(entry));
 
-  // Parametrize the video by passing arbitrary props to your component.
-  const inputProps = {
-    foo: "bar",
-  };
+  // // Parametrize the video by passing arbitrary props to your component.
+  // const inputProps = {
+  //   foo: "bar",
+  // };
 
-  // Extract all the compositions you have defined in your project
-  // from the webpack bundle.
-  const comps = await getCompositions(bundleLocation, {
-    // You can pass custom input props that you can retrieve using getInputProps()
-    // in the composition list. Use this if you want to dynamically set the duration or
-    // dimensions of the video.
-    inputProps,
-  });
+  // // Extract all the compositions you have defined in your project
+  // // from the webpack bundle.
+  // const comps = await getCompositions(bundleLocation, {
+  //   // You can pass custom input props that you can retrieve using getInputProps()
+  //   // in the composition list. Use this if you want to dynamically set the duration or
+  //   // dimensions of the video.
+  //   inputProps,
+  // });
 
-  console.log('comps', comps)
+  // console.log('comps', comps)
 
   // Select the composition you want to render.
-  const composition = comps.find((c) => c.id === compositionId);
-
-  // Ensure the composition exists
-  if (!composition) {
-    throw new Error(`No composition with the ID ${compositionId} found.
-  Review "${entry}" for the correct ID.`);
+  const composition = {
+    id: 'TitleOutlined-360x360-16fps',
+    defaultProps: { displayText: 'Render Test Text', style: {color: 'black'} },
+    durationInFrames: 55,
+    fps: 16,
+    height: 200,
+    width: 400,
   }
 
-  const outputLocation = `out/${compositionId}.mp4`;
+  const outputLocation = `out/${composition.id}.mp4`;
   console.log("Attempting to render:", outputLocation);
   await renderMedia({
     composition,
     serveUrl: bundleLocation,
     codec: "h264",
     outputLocation,
-    inputProps,
+    inputProps: composition.defaultProps,
   });
   console.log("Render done!");
 };
