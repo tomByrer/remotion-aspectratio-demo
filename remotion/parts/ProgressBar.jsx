b31d6ccb8381be230ac0dd8ed5d3912fa4dbfef9
tomByrer <tombyrer@gmail.com>
2022-07-09 01:46:12 -0500

    improve FrameCount, font fails in NextJS

--- remotion/parts/ProgressBar.jsx
+++ remotion/parts/ProgressBar.jsx
@@ -1,15 +1,17 @@
 import * as useConvert from '../helpers/useConvert'
 
 /* csscodelab.com/custom-react-js-progress-bar/ */
+//TODO have doneFixed in zindex 2 overlayering (vs inside) bar
 export const ProgressBar = ({
 	colorBackDone='black',
 	colorFrontDone='white',
 }) => {
 	const fontSize = useConvert.sizeFontSmall(69)
+	const toptweak = `${fontSize*0.095}px` //TODO perfect responsive
 	// book "Small Big": studies show people are motivated by progress half way, then % to compleation after half
 	let donePercent = useConvert.framesDonePercent() //WA assignment prevents crash after 50%
 	let isNumberRight = donePercent > 50
-	let doneFixed = donePercent.toFixed()
+	let doneFixed = donePercent.toFixed() //TODO float; see above
 
 	return (
 		<div class="progress" style={{
@@ -19,15 +21,18 @@ export const ProgressBar = ({
 			margin: "15 0",
 			height: useConvert.sizeFontSmall(88),
 			width: useConvert.vw(97),
+			fontFamily: 'Hamlet',
+			lineHeight: `${fontSize}px`,
 			fontSize: `${fontSize}px`,
 		}}>
 			{(!isNumberRight)
 				? <div class="progress-right" style={{
 						position:'fixed',
-						margin:`2px 0 0 ${donePercent+1}%`,
+						margin:`${toptweak} 0 0 ${donePercent+1}%`,
+						lineHeight: `${fontSize}px`,
 						fontSize:`${fontSize}px`,
 					}}>
-						{doneFixed +'%'}
+						<code>{doneFixed +'%'}</code>
 					</div>
 				: null
 			}
@@ -41,10 +46,11 @@ export const ProgressBar = ({
 				width: `${donePercent}%`,
 				color: colorFrontDone,
 				opacity: 1,
+				lineHeight: `${fontSize}px`,
 				// fontSize: `${fontSize}px`,
 			}}>
 				{(isNumberRight)
-					? <div style={{margin:`2px ${fontSize*.8}px 0 0`,}}>{doneFixed +'%'}</div>
+					? <div style={{lineHeight: `${fontSize}px`,margin:`${toptweak} ${fontSize*.8}px 0 0`}}><code>{doneFixed +'%'}</code></div>
 					: null
 				}
 			</div>
--- remotion/segments/FrameCount.jsx
+++ remotion/segments/FrameCount.jsx
@@ -1,8 +1,25 @@
-import { useCurrentFrame, useVideoConfig, Sequence, Series } from "remotion"
+import { useCurrentFrame, useVideoConfig, Sequence, Series,
+	continueRender, delayRender, staticFile } from "remotion"
 import * as useConvert from '../helpers/useConvert'
 
 import { ProgressBar } from 	'../parts/ProgressBar'
-import {TwoCenteredCode} from '../segments/TwoCenteredCode'
+import { TwoCenteredCode } from '../segments/TwoCenteredCode'
+
+// const waitForFont = delayRender()
+// const fileHahmlet = staticFile("hahmlet-vf.woff2")
+// console.log('fileHahmlet', fileHahmlet)
+// const hahmlet = new FontFace(
+//   `hahmlet`,
+//   `url(${fileHahmlet}) format('woff2')`
+// )
+// hahmlet
+//   .load()
+//   .then(() => {
+//     document.fonts.add(hahmlet);
+//     continueRender(waitForFont);
+//   })
+//   .catch((err) => console.log("Error loading font", err));
+
 
 export const FrameCount = ({
 	aspects={
@@ -30,7 +47,7 @@ export const FrameCount = ({
 	}
 	const FADED_TEXT = 0.618
 
-	aspects.rowTop = <span style={{opacity:FADED_TEXT}}>w:{useVideoConfig().width} h:{useVideoConfig().height} fps:{useVideoConfig().fps}</span>
+	aspects.rowTop = <span style={{fontFamily:'hahmlet', opacity:FADED_TEXT}}>w:{useVideoConfig().width} h:{useVideoConfig().height} fps:{useVideoConfig().fps}</span>
 
 	//MUST obtain frame count at top-level, before Sequence
 	// 1-index since humans start counting at one ;)
@@ -39,8 +56,8 @@ export const FrameCount = ({
 	aspects.codeLeft = FormatLeadingChar( useCurrentFrame() +1, FRAME_DIGITS, 0 )
 	const ROUNDOFF = 2
 	aspects.codeRight = FormatLeadingChar( useConvert.frames2seconds(useCurrentFrame(),+1), SEC_DIGITS, ROUNDOFF )
-	aspects.textLeft = <span style={{opacity:FADED_TEXT}}>frames of {useVideoConfig().durationInFrames}</span>
-	aspects.textRight = <span style={{opacity:FADED_TEXT}}>seconds of {useConvert.frames2seconds( useVideoConfig().durationInFrames )}</span>
+	aspects.textLeft = <span style={{fontFamily:'hahmlet', opacity:FADED_TEXT}}>frames of {useVideoConfig().durationInFrames}</span>
+	aspects.textRight = <span style={{fontFamily:'hahmlet', opacity:FADED_TEXT}}>seconds of {useConvert.frames2seconds( useVideoConfig().durationInFrames )}</span>
 
 	aspects.rowBottom = <ProgressBar colorBackDone={aspects.colorFront} />
 
